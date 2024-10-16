5889e2693b0b9373098c89663f89693b1cfc4ceb
Pravus696 <tbell3393@gmail.com>
2024-10-16 17:51:50 -0500

    about.jsx updated with information about myself

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,52 @@
 const About = () => {
-
+  <section id="about">
+    <h2>About Me:</h2>
+    <div className="about-segment">
+      <h3>Introduction:</h3>
+      <p>
+        My name is Trevor Bell. I&apos;m currently a student through UoW/edX,
+        where I&apos;m learning to increase my knowledge of full-stack web
+        development
+      </p>
+    </div>
+    <div className="about-segment">
+      <h3>Background:</h3>
+      <p>
+        I&apos;ve always had an interest in computers/coding, and primarily self
+        taught how to code for various tasks. This passion led me to self learn
+        VBA, and I decided to explore other options to increase my knowledge on
+        coding to allow me to turn my hobby into a professional pursuit.
+      </p>
+    </div>
+    <div className="about-segment">
+      <h3>Passion:</h3>
+      <p>
+        I am driven by my desire to learn as much as I can about computers and
+        coding to better understand how technology around me functions. My goal
+        is to create unique and engaging projects that will bring passion and
+        easier living to various communities.
+      </p>
+    </div>
+    <div className="about-segment">
+      <h3>Hobbies:</h3>
+      <p>
+        In my free time, I enjoy playing video games with my friends, spending
+        time with my cats, and researching farious facts about anything that
+        catches my eye. I enjoy not only technical sides of every aspect around
+        me, but also mechanical sides. I like to tinker and see how things work
+        to bring me ideas for creations that I would like to do in the future.
+      </p>
+    </div>
+    <div className="about-segment">
+      <h3>Personal Insight:</h3>
+      <p>
+        Struggling with my mental health in early stages of my life had brought
+        me to finding solace in my video game hobby. My interest in video games
+        and my desire to learn things lead to me diving into the understanding
+        of how machines and technology works around me.
+      </p>
+    </div>
+  </section>;
 };
+
+export default About;
