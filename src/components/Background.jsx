4538c67420b89a2f96dc3f5664cd5d5f8be3b9d7
Pravus696 <tbell3393@gmail.com>
2024-11-12 18:01:46 -0600

    created and installed vite framework

---
+++ src/components/Background.jsx
@@ -0,0 +1,42 @@
+const Resume = () => {
+  return (
+    <div>
+      <h1>Background</h1>
+      <h3>Education:</h3>
+      <div>
+        <ol>
+        <li>High School Diploma - 2014</li>
+        <li>Full Stack Web Development Bootcamp - 2024</li>
+        </ol>
+      </div>
+      <div>
+        <h3>Skills & Qualifications:</h3>
+        <ol>
+          <li>Various factory machines</li>
+          <li>Microsoft Suite</li>
+          <li>Effective written and communication skills</li>
+          <li>Quick decision making</li>
+          <li>Managing team of 80 associates</li>
+          <li>Multi-task and managing large workloads</li>
+          <li>Labor management</li>
+          <li>Basics of coding languages</li>
+        </ol>
+      </div>
+      <div>
+        <h3>Professional Experience:</h3>
+        <p>
+            I began working at a factory in 2014. I started as a general laborer 
+            for various different companies while building up my work experience.
+            I started working at Amazon in 2018 and worked my way up to a supervisor position. 
+            I managed a team of 80 associates and was responsible for ensuring that production 
+            goals were met. I also worked closely with the quality control department to ensure 
+            that all products met company standards. I have experience operating various factory 
+            machines and am proficient in Microsoft Suite. I am a quick learner and have excellent 
+            written and communication skills. I am also able to multi-task and manage large workloads. 
+            I am currently enrolled in a Full Stack Web Development Bootcamp to further my skills 
+            and knowledge in coding languages. I am excited to continue learning and growing in my career.
+        </p>
+      </div>
+    </div>
+  );
+};
