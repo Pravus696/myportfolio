cd78f16786fec621130c1b5776241d3e979c24fa
Pravus696 <tbell3393@gmail.com>
2024-11-25 18:15:15 -0600

    added 3 more projects to project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,10 @@ const Projects = () => {
     <section id="projects">
       <h2>Projects</h2>
       <div>
-        <div class="project-segment" style={{ backgroundImage: "url(/project1.jpg)" }}>
+        <div
+          class="project-segment"
+          style={{ backgroundImage: "url(/project1.jpg)" }}
+        >
           <h3>Professional README Generator</h3>
           <p>
             This project is one of my earlier works. It allows for an easy
@@ -14,7 +17,10 @@ const Projects = () => {
             View Project
           </a>
         </div>
-        <div class="project-segment" style={{ backgroundImage: "url(/project2.jpg)" }}>
+        <div
+          class="project-segment"
+          style={{ backgroundImage: "url(/project2.jpg)" }}
+        >
           <h3>Build Your Own Vehicle</h3>
           <p>
             This project allows a user to create and operate a vehicle within
@@ -26,7 +32,10 @@ const Projects = () => {
             View Project
           </a>
         </div>
-        <div class="project-segment" style={{ backgroundImage: "url(/project3.jpg)" }}>
+        <div
+          class="project-segment"
+          style={{ backgroundImage: "url(/project3.jpg)" }}
+        >
           <h3>Ultimate Video Game Suggestions</h3>
           <p>
             This was the first group project that I had worked on in our coding
@@ -36,6 +45,38 @@ const Projects = () => {
             functioning webpage.
           </p>
           <a href="https://github.com/Pravus696/uvgs">View Project</a>
+        </div>
+         <div class="project-segment" style={{ backgroundImage: "url(/project4.jpg)" }}>
+          <h3>Employee Tracker</h3>
+          <p>
+            This project uses PostgreSQL to allow employee tracking through the 
+            terminal. It was a fun project to work on and allowed me to explore 
+            the use of SQL in a project.
+          </p>
+          <a href="https://github.com/Pravus696/employeetracker">
+            View Project
+          </a>
+        </div>
+        <div class="project-segment" style={{ backgroundImage: "url(/project5.jpg)" }}>
+          <h3>Kanban Board</h3>
+          <p>
+            This project was a simple kanban board that allowed for the 
+            creation of tasks and moving them between different columns.
+            Giving a visual representation of the tasks that needed to be done
+            and what was currently being worked on or completed.
+          </p>
+          <a href="https://github.com/Pravus696/kanbanboard">
+            View Project
+          </a>
+        </div>
+        <div class="project-segment" style={{ backgroundImage: "url(/project6.jpg)" }}>
+          <h3>Social Networking API</h3>
+          <p>
+            This project was a fun one to work on. It allowed me to explore the
+            use of MongoDB and Mongoose to create a backend API for a social
+            networking site. I was able to learn a lot from it.
+          </p>
+          <a href="https://github.com/Pravus696/Social-Network-API">View Project</a>
         </div>
       </div>
     </section>
